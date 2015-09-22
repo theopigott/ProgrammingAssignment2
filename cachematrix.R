e117c0280e027cb962947605e0f7614836d2c02a
theopigott <theopigott@users.noreply.github.com>
2015-09-22 15:59:40 +0100

    Added comments, fixed lazy evaluation issue

--- cachematrix.R
+++ cachematrix.R
@@ -4,28 +4,37 @@
 ## to "get" and "set" value of x and its (cached) inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL
+        # Initialise matrix with value
+        # This avoids lazy evaluation issues by using argument x
+        set(x)
+        # Update matrix to y, reset inverse to null (unknown)
         set <- function(y) {
                 x <<- y
                 inv <<- NULL
         }
+        # Return value of matrix
         get <- function() x
-        setinverse <- function(inverse) inv <<- inverse
-        getinverse <- function() inv
-        list(set = set, get = get, setinverse = setinverse, 
-             getinverse = getinverse)
+        # Update inverse
+        setInverse <- function(inverse) inv <<- inverse
+        # Return inverse
+        getInverse <- function() inv
+        list(set = set, get = get, setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
 
-## Find the inverse of x, using the cached inverse if x hasn't
-## changed since cacheSolve was last called. Otherwise, 
-## calculate the inverse of x and cache it.
+## Find inverse of x using cached value if x hasn't changed since cacheSolve
+## was last called. Otherwise, calculate the inverse of x and cache it.
 
 cacheSolve <- function(x, ...) {
-        inv <- x$getinverse()
+        inv <- x$getInverse()
+        # If inv not null it's already been calculated so return cached value
         if (!is.null(inv)) return(inv)
+        # Otherwise calculate inverse ...
         mat <- x$get()
         inv <- solve(mat, ...)
-        x$setinverse(inv)
+        # ... cache it ...
+        x$setInverse(inv)
+        # ... and return it
         inv
 }
